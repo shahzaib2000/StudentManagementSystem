c9200d63e4ccae510770602797bdc559a00a14a8
beast <beast@LAPTOP-0G1FUQD9>
2023-09-27 00:12:23 +0500

    Add project files.

---
+++ StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetStudent([FromQuery] int count)
+        {
+            string[] names = { "Shahzaib", "Ali", "Samreen" };
+            if (!names.Any())
+                return NotFound();
+            return Ok(names.Take(count));
+            //return Ok(names);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteStudent(string id)
+        {
+            bool badresponse = false;
+
+            if (badresponse)
+
+                return BadRequest();
+
+            return NoContent();
+        }
+
+    }
+}
